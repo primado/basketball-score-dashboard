3b4417f5e3bce60f7b887a74ca9f456c9914f93d
primado <primadoprincepa@gmail.com>
2023-01-27 14:18:09 +0000

    add screen breakpoints

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,25 +8,25 @@ export default function Dashboard() {
 
     return (
         <div>
-            <section className="bg-bg_dark_grey py-32 w-screen h-sc h-screen">
+            <section className="bg-bg_dark_grey py-32 w-screen h-sc h-screen sm-412:px-8 sm-428:px-8 2xl-1440:py-20 lg-1024:py-16">
                 <div className="main-container">
-                    <h1 className="text-[#fff] text-3xl text-center font-roboto font-extrabold mb-5">Dashboard</h1>
+                    <h1 className="text-[#fff] text-3xl text-center font-roboto font-extrabold mb-12 sm-375:mb-8 sm-425:text-base 2xl-1440:text-5xl 3xl-2560:text-5xl">Dashboard</h1>
 
                     <div className="container grid grid-cols-2 gap-20 place-items-center justify-items-center">
 
                         <div className="img-container">
-                            <img className="w-1/4 block border border-[#fff] m-auto"
+                            <img className="w-1/4 block border border-[#fff] m-auto sm-412:w-40 md-810:w-52"
                             src={court} alt="Basket Ball Court"/>
                         </div>
 
                         <div className="shot-container">
                             <div className="shot">
-                                <h2 className="text-[#fff] text-xl mb-2 font-roboto font-semibold "
+                                <h2 className="text-[#fff] text-xl mb-2 font-roboto font-semibold sm-412:text-lg"
                                 > SHOTS
                                 </h2>
 
                                 <div 
-                                className="shot-data text-[#000] text-center font-black font-roboto text-4xl bg-bg_white w-40 py-10 max-w-[10rem]"
+                                className="shot-data text-[#000] text-center font-black font-roboto text-4xl bg-bg_white flex justify-center items-center w-40 h-20 max-w-[10rem] sm-412:w-28 max-h-20 sm-412:m-w-[7rem] sm-412:h-20 sm-428:w-32 sm-428:max-w-[8rem]"
                                 >
                                     3
                                 </div>
@@ -36,21 +36,23 @@ export default function Dashboard() {
                         <div className="score-container">
                             <div className="score">
 
-                                <h2 className="text-[#fff] text-xl mb-2 font-roboto font-semibold"
+                                <h2 className="text-[#fff] text-xl mb-2 font-roboto font-semibold sm-412:text-lg"
                                 > SCORE
                                 </h2>
 
-                                <div className="score-data  text-[#000] font-black font-roboto text-center text-4xl  bg-bg_white w-40 py-10 max-w-[10rem]">
+                                <div className="score-data  text-[#000] font-black font-roboto text-center text-4xl  bg-bg_white flex justify-center items-center w-40 h-40  max-w-[10rem] sm-412:w-28 sm-412:max-w-[7rem] sm-412:h-28 sm-428:w-32 sm-428:max-w-[8rem] sm-428:max-h-28"
+                                >
                                     00
                                 </div>
                             </div>
                         </div>
 
                         <div className="clock">
-                            <h2 className="text-[#fff] text-xl font-roboto font-semibold mb-2"
-                            > Shot Clock
+                            <h2 className="text-[#fff] text-xl font-roboto font-semibold mb-2 sm-412:text-lg"
+                            > SHOT CLOCK
                             </h2>
-                            <div className="clock-data text-[#000] font-roboto font-black text-center text-4xl bg-bg_white w-40 py-10 max-w-[10rem]">
+                            <div className="clock-data text-[#000] font-roboto font-black text-center text-4xl bg-bg_white flex justify-center items-center w-40 h-40 max-w-[10rem] max-h-40 sm-412:w-28 sm-412:max-w-[7rem] sm-428:w-32 sm-428:max-w-[8rem] sm-412:h-28 sm-428:max-h-28"
+                            >
                                 50
                             </div>
                         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,6 +21,27 @@ module.exports = {
       roboto: ['Roboto', 'sans-serif'],
     
     },
+
+    screens: {
+      'sm-412': {'max': '420px'},
+      'sm-414': {'max': '418px'},
+      'sm-425': {'max': '428px'},
+      'sm-360': {'max': '380px'},
+      'sm-320': {'max': '330px'},
+      'sm-375': {'max': '380px'},
+
+      'sm-428': {'max': '435px'},
+      'sm-390': {'max': '400px'},
+
+      'md-810': {'max': '820px'},
+      'md-690': {'max': '700px'},
+
+      'lg-1024': {'max': '1024px'},
+      '2xl-1440': {'max': '1450px'},
+      '3xl-2560': {'max': '2570px'},
+
+
+    }
   },
 
 
