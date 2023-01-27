cc3a31fd9f5600518ca10a67768a3a362e32204a
primado <primadoprincepa@gmail.com>
2023-01-27 16:31:18 +0000

    some changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,25 +8,25 @@ export default function Dashboard() {
 
     return (
         <div>
-            <section className="bg-bg_dark_grey py-24 w-screen h-sc h-screen sm-412:px-8 sm-428:px-8 2xl-1440:py-20 lg-1024:py-16">
+            <section className="bg-bg_dark_grey py-24 w-full h-full sm-412:h-screen sm-412:w-screen sm-412:px-12 sm-412:py-20 sm-428:px-12 sm-428:py-20 sm-428:w-screen sm-428:h-screen md-810:h-screen md-768:w-screen md-768:h-screen md-900:w-screen md-900:h-screen">
                 <div className="main-container">
                     <h1 className="text-[#fff] text-3xl text-center font-roboto font-extrabold mb-12 sm-375:mb-8 sm-425:text-base 2xl-1440:text-5xl 3xl-2560:text-5xl">Dashboard</h1>
 
                     <div className="container grid grid-cols-2 gap-20 place-items-center justify-items-center">
 
                         <div className="img-container">
-                            <img className="w-1/4 block border border-[#fff] m-auto sm-412:w-40 md-810:w-52"
+                            <img className="w-1/4 block border border-[#fff] m-auto sm-360:w-48 sm-412:w-48 md-810:w-52"
                             src={court} alt="Basket Ball Court"/>
                         </div>
 
                         <div className="shot-container">
                             <div className="shot">
-                                <h2 className="text-[#fff] text-xl mb-2 font-roboto font-semibold sm-412:text-lg"
+                                <h2 className="text-[#fff] text-xl mb-2 font-roboto font-semibold "
                                 > SHOTS
                                 </h2>
 
                                 <div 
-                                className="shot-data text-[#000] text-center font-black font-roboto text-4xl bg-bg_white flex justify-center items-center w-40 h-20 max-w-[10rem] sm-412:w-28 max-h-20 sm-412:m-w-[7rem] sm-412:h-20 sm-428:w-32 sm-428:max-w-[8rem]"
+                                className="shot-data text-[#000] text-center font-black font-roboto text-9xl bg-bg_white flex justify-center items-center w-52 h-48 max-w-[13rem] max-h-48 sm-412:w-36 sm-412:h-40 sm-412:text-5xl sm-428:w-36 sm-428:h-40 sm-428:text-5xl"
                                 >
                                     3
                                 </div>
@@ -40,7 +40,7 @@ export default function Dashboard() {
                                 > SCORE
                                 </h2>
 
-                                <div className="score-data  text-[#000] font-black font-roboto text-center text-4xl  bg-bg_white flex justify-center items-center w-40 h-40  max-w-[10rem] sm-412:w-28 sm-412:max-w-[7rem] sm-412:h-28 sm-428:w-32 sm-428:max-w-[8rem] sm-428:max-h-28"
+                                <div className="score-data  text-[#000] font-black font-roboto text-center text-9xl  bg-bg_white flex justify-center items-center w-72 h-60 max-w-[18rem] max-h-60 sm-412:w-36 sm-412:h-40 sm-412:text-5xl sm-428:w-36 sm-428:h-40 sm-428:text-5xl"
                                 >
                                     00
                                 </div>
@@ -51,7 +51,7 @@ export default function Dashboard() {
                             <h2 className="text-[#fff] text-xl font-roboto font-semibold mb-2 sm-412:text-lg"
                             > SHOT CLOCK
                             </h2>
-                            <div className="clock-data text-[#000] font-roboto font-black text-center text-4xl bg-bg_white flex justify-center items-center w-40 h-40 max-w-[10rem] max-h-40 sm-412:w-28 sm-412:max-w-[7rem] sm-428:w-32 sm-428:max-w-[8rem] sm-412:h-28 sm-428:max-h-28"
+                            <div className="clock-data text-[#000] font-roboto font-black text-center text-9xl bg-bg_white flex justify-center items-center w-72 h-60 max-w-[18rem] max-h-60 sm-412:w-36 sm-412:h-40 sm-412:text-5xl sm-428:w-36 sm-428:h-40 sm-428:text-5xl"
                             >
                                 50
                             </div>
