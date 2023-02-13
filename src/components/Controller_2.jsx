019f5cbf63e83df4660bc10442764274a4aa04f7
primado <primadoprincepa@gmail.com>
2023-02-13 08:13:41 +0000

    add some changes

--- src/components/Controller_2.jsx
+++ src/components/Controller_2.jsx
@@ -135,7 +135,7 @@ export default function Controller_2() {
                     <h1 className="font-montserrat text-white font-bold text-3xl">GAME CONTROLLER</h1>
                 </div>
 
-                <div className="bg-white w-2/4 py-8 md-1080:w-4/6 md-810:w-10/12">
+                <div className="bg-white w-2/4 pt-12 h-96 md-1080:w-4/6 md-810:w-10/12">
 
                     <div className="settings flex flex-row justify-between px-8 mb-8">
                         
@@ -147,21 +147,21 @@ export default function Controller_2() {
                         </div>
 
                         <div className="dropdown">
-      <div className="selected-option" onClick={toggleOptions}>
-        {selectedOption}
-      </div>
-      {showOptions ? (
-        <ul className="options">
-          <li>
-            <Link to="/config">
-              <div>Configuration</div>
-            </Link>
-          </li>
-          <li onClick={() => selectOption("single")}>Single Player</li>
-          <li onClick={() => selectOption("multi")}>MultiPlayer</li>
-        </ul>
-      ) : null}
-    </div>
+                          <div onClick={toggleOptions}  className="selected-option rounded-lg border-2 font-montserrat font-semibold text-lg">
+                            {selectedOption}
+                          </div>
+                          {showOptions ? (
+                            <ul className="options">
+                              <li>
+                                <Link to="/config" className="text-base font-montserrat font-semibold text-black hover:text-blue-700">
+                                  Configuration
+                                </Link>
+                              </li>
+                              <li onClick={() => selectOption("single")} className="text-base font-montserrat font-semibold hover:text-blue-700">Single Player</li>
+                              <li onClick={() => selectOption("multi")} className="text-base font-montserrat font-semibold hover:text-blue-700">MultiPlayer</li>
+                            </ul>
+                          ) : null}
+                        </div>
                     </div>
 
                     <div className="">
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -166,29 +166,29 @@ export default function Dashboard() {
 
     return (
         <div>
-            <section className="bg-home-bg bg-no-repeat bg-center bg-cover min-h-screen w-full sm-412:h-screen sm-412:w-screen sm-412:px-12 sm-412:py-20 sm-428:px-12 sm-428:py-20 sm-428:w-screen sm-428:h-screen md-810:h-screen md-768:w-screen md-768:h-screen md-900:w-screen md-900:h-screen md-810:py-20 md-1080:py-20">
+            <section className="bg-home-bg bg-no-repeat bg-center bg-cover py-14 min-h-screen w-full sm-412:h-screen sm-412:w-screen sm-412:px-12 sm-412:py-20 sm-428:px-12 sm-428:py-20 sm-428:w-screen sm-428:h-screen md-810:h-screen md-768:w-screen md-768:h-screen md-900:w-screen md-900:h-screen md-810:py-20 md-1080:py-20">
                 <div className="main-container">
               <h1 className="text-[#fff] text-6xl text-center font-roboto font-extrabold mb-12 sm-375:mb-8 sm-425:text-base 2xl-1440:text-5xl 3xl-2560:text-5xl">SMART HOOP 
                {showPopup ? (
               <Popup open={showPopup} position="center" >
                 <div className="gameover" >GAMEOVER</div>
               </Popup>
-      ) : null}
-       {showPopup1 ? (
-              <Popup open={showPopup1} position="center" >
-                <div className="execellent" >Execellent Shot</div>
-              </Popup>
-      ) : null}
-      {showPopup2 ? (
-              <Popup open={showPopup2} position="center" >
-                <div className="execellent" >Bank Shot</div>
-              </Popup>
-      ) : null}
-      {showPopup3 ? (
-              <Popup open={showPopup3} position="center" >
-                <div className="execellent" >MISS</div>
-              </Popup>
-      ) : null}
+              ) : null}
+               {showPopup1 ? (
+                      <Popup open={showPopup1} position="center" >
+                        <div className="execellent" >Execellent Shot</div>
+                      </Popup>
+              ) : null}
+              {showPopup2 ? (
+                      <Popup open={showPopup2} position="center" >
+                        <div className="execellent" >Bank Shot</div>
+                      </Popup>
+              ) : null}
+              {showPopup3 ? (
+                      <Popup open={showPopup3} position="center" >
+                        <div className="execellent" >MISS</div>
+                      </Popup>
+              ) : null}
       
                     {/* <button onClick={handleStartStop}>
                         {isActive ? "." : "."}
